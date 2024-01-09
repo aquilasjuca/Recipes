fb52b9efddbb4262acd4e62237e3921a56ce5443
Áquilas Rodrigues Jucá <aquilasjuca2016@gmail.com>
2024-01-08 21:00:26 -0300

    Criando algumas urls e o app recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def my_view(request):
-    return HttpResponse('Olá Django!')
+def home(request):
+    return HttpResponse('Home')
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+def contato(request):
+    return HttpResponse('Contato')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', my_view),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
 ]
